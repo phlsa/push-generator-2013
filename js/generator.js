81671bc45508da1e89dea5c6b4a0f958eedf7cc4
Philipp Sackl <philipp@envis-precisely.com>
2013-10-01 19:51:08 +0200

    initial commit

---
+++ js/generator.js
@@ -0,0 +1,82 @@
+var canvas = document.getElementById( 'canv' );
+var ctx = canv.getContext( '2d' );
+
+var theText = "";
+
+var buildGraph = function( text, p ) {
+  text = text.toLowerCase();
+  var w = p.width,
+      h = p.height;
+  var step = w / text.length;
+  var max = maxPercentage( text );
+  
+  // draw the graph
+  _.each( languageData.languageName, function( name, langIndex ) {
+    p.fill( 194, 0, 83, 25 );
+    p.stroke( 255, 25 );
+    p.beginShape();
+    p.vertex( 0, h );
+    _.each( text, function( letter, index ) {
+      p.ellipse( step*index, h-getFrequency(letter,langIndex)/max*h, 10, 10 );
+      p.vertex( step*index, h-getFrequency(letter,langIndex)/max*h );
+    });
+    p.vertex( w, h );
+    p.vertex( 0, h );
+    p.endShape();
+  });
+}
+
+/***
+ * Extract the frequency of a single letter
+ **/
+var getFrequency = function( letter, language ) {
+  if ( languageData[letter] === undefined ) {
+    return 0;
+  }
+
+  if ( language === undefined ) {
+    return languageData[letter];
+  } else {
+    return languageData[letter][language];
+  }
+}
+
+/***
+ * Calculate maximum letter frequency of any letter within @text
+ **/
+var maxPercentage = function( text, language ) {
+  if ( language === undefined ) {
+    // when no language is defined, return the maximum of any letter in the text in any language
+    return _.max( _.flatten( _.map( text, function( letter ) {
+        if ( languageData[letter] === undefined ) {
+          return 0;
+        }
+        return languageData[letter];
+    })));
+  } else {
+    // if a language is defined, look directly into that language
+    return _.max( _.map( text, function( letter ) {
+      if ( languageData[letter] === undefined ) {
+        return 0;
+      }
+      return languageData[letter][language];
+    }));
+  }
+}
+
+/***
+ * Create Processing instance
+ **/
+var proc = new Processing( canvas, function( p ) {
+    p.setup = function() {
+      p.size( 600, 600 );
+      p.background( 250 );
+    }
+    
+    p.draw = function() {
+      ctx.globalCompositeOperation = 'copy';
+      p.background( 51 );
+      ctx.globalCompositeOperation = 'lighter';
+      buildGraph( theText, p );
+    }
+});
